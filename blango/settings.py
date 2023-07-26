a9239c0509cc4b3702805d5a996fe6c8160edfd5
Krzysztof Sikora <krzyssikora@gmail.com>
2023-07-26 21:52:54 +0000

    finished custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -38,6 +38,7 @@ class Dev(Configuration):
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
+    AUTH_USER_MODEL = "blango_auth.User"
 
     # Application definition
 
@@ -48,6 +49,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
